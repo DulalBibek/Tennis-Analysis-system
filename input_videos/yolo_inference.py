7893e56d6fee8806703ff2575c67b26a2cbae115
Bibek Dulal <Bibekdulal54@gmail.com>
2026-08-09 15:46:45 +0100

    updated conf score for yolo

--- input_videos/yolo_inference.py
+++ input_videos/yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model=YOLO('/Users/bibekdulal/Desktop/Tennis_analysis/models/weights/last.pt')
 
-result=model.predict('/Users/bibekdulal/Desktop/Tennis_analysis/input_videos/input_video.mp4',conf=0.4, save=True)
+result=model.predict('/Users/bibekdulal/Desktop/Tennis_analysis/input_videos/input_video.mp4',conf=0.2, save=True)
 
 print(result)
 
